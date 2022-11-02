bb14a10e80c04a65dd9315fd8cf65f518100d69e
liuwenzheng <329541594@qq.com>
2022-11-02 11:15:19 +0800

    更新编译版本和权限申请方式

--- app/src/main/java/com/moko/lw003/activity/DeviceInfoActivity.java
+++ app/src/main/java/com/moko/lw003/activity/DeviceInfoActivity.java
@@ -525,6 +525,7 @@ public class DeviceInfoActivity extends BaseActivity implements RadioGroup.OnChe
                     final DfuServiceInitiator starter = new DfuServiceInitiator(mDeviceMac)
                             .setDeviceName(mDeviceName)
                             .setKeepBond(false)
+                            .setForeground(false)
                             .setDisableNotification(true);
                     starter.setZip(null, firmwareFilePath);
                     starter.start(this, DfuService.class);
